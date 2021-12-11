31c52849a1fd83ebe6d2f28ccd715eea42ead1cd
Andrey Lukyantsev <al@4010.ru>
2021-12-12 02:49:08 +0300

    Функции стекинга дописаны для случая регрессии

--- ml/stacking.py
+++ ml/stacking.py
@@ -10,10 +10,18 @@ from . model_scoring import *
 # stacking_coef(y_valid.values, [y_valid_xgb_norma, y_valid_ctb_norma, y_valid_lgb_norma], scoring_f=f1_score)
 # TODO сделать подбор коэффициентов через рекурсию
 # TODO сейчас работает в один поток, попробовать распараллелить
-def stacking_coef(y_true, y_preds, scoring_f=accuracy_score, range_from=0, range_max=0.5, range_step=0.1):
+def stacking_coef(
+    y_true, y_preds,
+    scoring_f=accuracy_score, greater_is_better=True,
+    range_from=0, range_max=0.5, range_step=0.1,
+    threshold=False
+):
 
     count_range = 0
-    res = pd.DataFrame(columns=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'threshold', 'score'])
+    if threshold:
+        res = pd.DataFrame(columns=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'threshold', 'score'])
+    else:
+        res = pd.DataFrame(columns=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'score'])
     
     t_from, t_max = [], []
     for t in range(9):
@@ -34,7 +42,9 @@ def stacking_coef(y_true, y_preds, scoring_f=accuracy_score, range_from=0, range
                                 for h in np.arange(t_from[7], t_max[7], range_step):
                                     for i in np.arange(t_from[8], t_max[8], range_step):
                                         if a+b+c+d+e+f+g+h+i == 1:
-                                            for z in np.arange (range_step, 1, range_step):
+                                            
+                                            range_z = np.arange (range_step, 1, range_step) if threshold else [-1]
+                                            for z in range_z:
 
                                                 count_range += 1
                                                 y_pred = [0]*len(y_preds[0])
@@ -44,60 +54,87 @@ def stacking_coef(y_true, y_preds, scoring_f=accuracy_score, range_from=0, range
                                                 for y in y_preds:
                                                     y_pred += y*counters[y_i]
                                                     y_i += 1
-                                                y_pred = (y_pred > z).astype(int)
+                                                if threshold:
+                                                    y_pred = (y_pred > z).astype(int)
                                                 score = scoring_f(y_true, y_pred)
 
-                                                r = pd.Series(data={
-                                                    'a':a, 'b':b, 'c':c, 'd':d, 'e':e, 'f':f, 'g':g, 'h':h, 'i':i, \
-                                                    'threshold':z, 'score':score
-                                                }, name=count_range)
+                                                if threshold:
+                                                    r = pd.Series(data={
+                                                        'a':a, 'b':b, 'c':c, 'd':d, 'e':e, 'f':f, 'g':g, 'h':h, 'i':i, \
+                                                        'threshold':z, 'score':score
+                                                    }, name=count_range)
+                                                else:
+                                                    r = pd.Series(data={
+                                                        'a':a, 'b':b, 'c':c, 'd':d, 'e':e, 'f':f, 'g':g, 'h':h, 'i':i, \
+                                                        'score':score
+                                                    }, name=count_range)
                                                 res = res.append(r)
 
-    return res[ res['score'] == res['score'].max() ]
+    if greater_is_better:
+        return res[ res['score'] == res['score'].max() ]
+    else:
+        return res[ res['score'] == res['score'].min() ]
 
 # собирает комбинированный предикт на основе предиктов нескольких моделей
 def stacking_pred(
-    df_stacking, stacking_valid_pred_list, stacking_pred_list, y_valid, y_test=[],
-    stacking_coef_first=np.nan, mute=False
+    df_stacking, stacking_valid_pred_list, stacking_pred_list,
+    y_valid, y_test=[],
+    scoring_f=accuracy_score, greater_is_better=True,
+    stacking_coef_first=np.nan, mute=False, threshold=False
 ):
 
     # вытаскиваем парметры из переданного датафрейма
     stacking_coef_first = stacking_coef_first if not math.isnan(stacking_coef_first) else df_stacking[:1].index[0]
+    if threshold:
+        stacking_drop_list = ['threshold', 'score']
+    else:
+        stacking_drop_list = ['score']
     stacking_coef_list = df_stacking[ df_stacking.index == stacking_coef_first ]. \
-                        drop(['threshold', 'score'], axis=1). \
+                        drop(stacking_drop_list, axis=1). \
                         values[0][0:len(stacking_valid_pred_list)][np.newaxis, :].T
-    stacking_threshold = df_stacking[ df_stacking.index == stacking_coef_first ]['threshold'].values[0]
+    if threshold:
+        stacking_threshold = df_stacking[ df_stacking.index == stacking_coef_first ]['threshold'].values[0]
 
     # смотрим какой получился прогноз на валидационных данных
     y_valid_pred = pd.Series( (stacking_valid_pred_list * stacking_coef_list).sum(axis=0) )
     if not mute:
         print('='*20 + ' Valid data ' + '='*21 + '\n')
-        print('Stage 1 valid predict:\n')
-        print(y_valid_pred.value_counts().sort_index())
+        if threshold:
+            print('Stage 1 valid predict:\n')
+            print(y_valid_pred.value_counts().sort_index())
+        else:
+            print('Best valid score: %f' % scoring_f(y_valid, y_valid_pred))
 
-    y_valid_pred = (y_valid_pred > stacking_threshold).astype(int)
-    if not mute:
-        print('\nStage 2 valid threshold %f:\n' % stacking_threshold)
-        print(y_valid_pred.value_counts())
-        print('\nStage 2 classification report:\n')
-        print(classification_report(y_valid, y_valid_pred))
-        skplt.metrics.plot_confusion_matrix(y_valid, y_valid_pred, normalize=False)
-        plt.show()
+    if threshold:
+        y_valid_pred = (y_valid_pred > stacking_threshold).astype(int)
+        if not mute:
+            print('\nStage 2 valid threshold %f:\n' % stacking_threshold)
+            print(y_valid_pred.value_counts())
+            print('\nStage 2 classification report:\n')
+            print(classification_report(y_valid, y_valid_pred))
+            skplt.metrics.plot_confusion_matrix(y_valid, y_valid_pred, normalize=False)
+            plt.show()
 
     # смотрим на тестовые данные
     y_pred = pd.Series( (stacking_pred_list * stacking_coef_list).sum(axis=0) )
     if not mute:
-        print('='*20 + ' Test data ' + '='*21 + '\n')
-        print('Stage 1 test predict:\n')
-        print(y_pred.value_counts().sort_index())
+        if threshold:
+            print('='*20 + ' Test data ' + '='*21 + '\n')
+            print('Stage 1 test predict:\n')
+            print(y_pred.value_counts().sort_index())
+        else:
+            if len(y_test) > 0:
+                print('='*20 + ' Test data ' + '='*21 + '\n')
+                print('Best test score: %f' % scoring_f(y_test, y_pred))
 
-    y_pred = (y_pred > stacking_threshold).astype(int)
-    if not mute:
-        print('\nStage 2 valid threshold %f:\n' % stacking_threshold)
-        print(y_pred.value_counts())
-        if len(y_test) > 0:
-            print(classification_report(y_test, y_pred))
-            skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=False)
-            plt.show()
+    if threshold:
+        y_pred = (y_pred > stacking_threshold).astype(int)
+        if not mute:
+            print('\nStage 2 valid threshold %f:\n' % stacking_threshold)
+            print(y_pred.value_counts())
+            if len(y_test) > 0:
+                print(classification_report(y_test, y_pred))
+                skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=False)
+                plt.show()
 
     return y_valid_pred, y_pred
